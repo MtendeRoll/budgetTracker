a5dbf9b0c4c433599f5fc531eaade42adde74eae
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-08-31 13:56:54 -0400

    added web manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const APP_PREFIX = "BudgetEvent-";
+const APP_PREFIX = "TransactionEvent-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = ["./index.html", "./css/styles.css"];
